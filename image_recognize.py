b9aade651b4d5ea72a67a7a131ebe9b2faba43bf
tim <timrhymer@ya.ru>
2022-10-18 21:08:57 +0200

    init

--- image_recognize.py
+++ image_recognize.py
@@ -1,10 +1,11 @@
 import cv2
 import pytesseract
 
+config = 'linebox'
 
 img = cv2.imread('1.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-text = pytesseract.image_to_string(img)
+text = pytesseract.image_to_string(img, config=config)
 
 print(text)
